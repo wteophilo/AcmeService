95a9d1c73acd9fc9c378f303c062baa929a27e13
WT <wteophilo@gmail.com>
2015-09-01 21:36:01 -0300

    Serviço ProfessorCurso

---
+++ AcmeService/IProfessorCursoService.cs
@@ -0,0 +1,28 @@
+﻿using AcmeService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AcmeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IProfessorCursoService" in both code and config file together.
+    [ServiceContract]
+    public interface IProfessorCursoService
+    {
+        [OperationContract]
+        void adiciona(ProfessorCurso professorCurso);
+        [OperationContract]
+        void remove(ProfessorCurso professorCurso);
+        [OperationContract]
+        void atualiza(ProfessorCurso professorCurso);
+        [OperationContract]
+        ProfessorCurso procura(int? id);
+        [OperationContract]
+        ICollection<ProfessorCurso> lista();
+        [OperationContract]
+        ProfessorCurso detalhe(int? id);
+    }
+}
---
+++ AcmeService/ProfessorCursoService.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using AcmeService.Model;
+using AcmeService.DAL;
+using System.Data.Entity;
+
+namespace AcmeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProfessorCursoService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ProfessorCursoService.svc or ProfessorCursoService.svc.cs at the Solution Explorer and start debugging.
+    public class ProfessorCursoService : IProfessorCursoService
+    {
+        private AppDBContext db = new AppDBContext();
+
+        public void adiciona(ProfessorCurso professorCurso)
+        {
+            db.Leciona.Add(professorCurso);
+            db.SaveChanges();
+        }
+
+        public void atualiza(ProfessorCurso professorCurso)
+        {
+            db.Entry(professorCurso).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public ProfessorCurso detalhe(int? id)
+        {
+            ProfessorCurso professorCurso = db.Leciona.Include(i => i.professor).SingleOrDefault(x => x.id == id);
+            return professorCurso;
+
+        }
+
+        public ICollection<ProfessorCurso> lista()
+        {
+            return db.Leciona.ToList();
+        }
+
+        public ProfessorCurso procura(int? id)
+        {
+            return db.Leciona.Find(id);
+        }
+
+        public void remove(ProfessorCurso professorCurso)
+        {
+            db.Leciona.Remove(professorCurso);
+            db.SaveChanges();
+        }
+    }
+}
